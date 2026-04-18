de3bdf69b863ddf86150bdfb4f801634a2bc30b9
varya <arzvar29@gmail.com>
2026-04-18 18:50:04 +0300

    add the label

--- task_window.py
+++ task_window.py
@@ -14,13 +14,14 @@ def create_second_window(parent):
     win.transient(parent)
 
     # Добавляем элементы
-
+    name_tsk = tk.Label(win, text="Введите имя задания")
+    name_tsk.place(x=20)
     btn_close = tk.Button(win, text="Закрыть", command=win.destroy)
     btn_close.place(x = 20, y = 160)
     name = tk.Entry(win, width=40)
-    name.place(x=20)
-    btn = tk.Button(win, text="hello")
-    btn.place(x=90, y=160)
+    name.place(x=20,y=40)
+    btn_Ok = tk.Button(win, text="Ok")
+    btn_Ok.place(x=90, y=160)
 
     # Возвращаем объект окна (не обязательно, но полезно)
     return win
