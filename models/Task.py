4695f9259a2cbb8d5e207e7848b8f875a1e8d061
varya <arzvar29@gmail.com>
2026-08-19 19:34:49 +0300

    add test and task, apd doc

--- models/Task.py
+++ models/Task.py
@@ -1,1 +1,12 @@
-class Task: pass
+class Task:
+    def __init__(self, question, answer, level):
+        self.question = question
+        self.answer = answer
+        self.level = level
+
+    def __str__(self):
+        s = f"{{'question': '{self.question}', 'answer': '{self.answer}', 'level': {self.level}}}"
+        return s
+
+# t = Task("революцио..ый", "революционный", 3)
+# print(t)
--- models/Test.py
+++ models/Test.py
@@ -1,1 +1,28 @@
-class Test: pass
+from models.Task import Task
+
+
+class Test:
+    def __init__(self, name):
+        self.name = name
+        self.list_tasks = []
+
+    def add_task(self, task):
+        self.list_tasks.append(task)
+
+    def count_task(self):
+        return len(self.list_tasks)
+
+    def __str__(self):
+        s=[]
+        for i in self.list_tasks:
+            s.append(str(i))
+        s= ", ".join(s)
+        return "["+s+"]"
+
+t1 = Task("революцио..ый", "революционный", 3)
+t2 = Task("не..видеть", "не видеть", 1)
+tt = Test("test")
+tt.add_task(t1)
+tt.add_task(t2)
+print(tt)
+print(tt.count_task())
